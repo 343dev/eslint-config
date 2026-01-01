6f5a68648b13c93e4f083ee027fb1cad521d0641
343dev <343dev@users.noreply.github.com>
2026-01-01 13:06:58 +0700

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,74 @@
 import js from '@eslint/js';
-import xo from 'eslint-config-xo';
-import deMorgan from 'eslint-plugin-de-morgan'; // eslint-disable-line import/no-unresolved
-import pluginImport from 'eslint-plugin-import';
-import pluginUnicorn from 'eslint-plugin-unicorn';
+import stylistic from '@stylistic/eslint-plugin';
+import eslintPluginDeMorgan from 'eslint-plugin-de-morgan';
+import eslintPluginImportLite from 'eslint-plugin-import-lite';
+import eslintPluginN from 'eslint-plugin-n';
+import eslintPluginRegexp from 'eslint-plugin-regexp';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
 import globals from 'globals';
 
-export default [
-	...xo,
-	js.configs.recommended,
-	deMorgan.configs.recommended,
-	pluginImport.flatConfigs.recommended,
-	pluginUnicorn.configs.recommended,
+const deMorganConfig = [
+	eslintPluginDeMorgan.configs.recommended,
+];
 
+const importConfig = [
+	eslintPluginImportLite.configs.recommended,
+];
+
+const jsConfig = [
+	js.configs.recommended,
+];
+
+const nConfig = [
+	eslintPluginN.configs['flat/recommended'],
+];
+
+const regexpConfig = [
+	eslintPluginRegexp.configs['flat/recommended'],
+];
+
+const stylisticConfig = [
+	stylistic.configs.recommended,
+	{
+		rules: {
+			'@stylistic/brace-style': ['error', '1tbs'],
+			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/no-tabs': 'off',
+			'@stylistic/semi': ['error', 'always'],
+		},
+	},
+];
+
+const unicornConfig = [
+	eslintPluginUnicorn.configs.recommended,
+	{
+		rules: {
+			'unicorn/no-process-exit': 'off',
+		},
+	},
+];
+
+export default [
 	{
 		languageOptions: {
-			globals: globals.builtin,
+			globals: {
+				...globals.es2021,
+				...globals.nodeBuiltin,
+			},
 			ecmaVersion: 'latest',
 			sourceType: 'module',
 		},
-
-		rules: {
-			'@stylistic/function-paren-newline': ['error', 'consistent'],
-			'@stylistic/object-curly-spacing': ['error', 'always'],
-			'capitalized-comments': 'off',
-			'no-warning-comments': 'off',
-			'unicorn/no-array-reduce': 'off',
+		linterOptions: {
+			reportUnusedDisableDirectives: 'error',
+			reportUnusedInlineConfigs: 'error',
 		},
 	},
+
+	...unicornConfig,
+	...deMorganConfig,
+	...regexpConfig,
+	...nConfig,
+	...stylisticConfig,
+	...jsConfig,
+	...importConfig,
 ];
